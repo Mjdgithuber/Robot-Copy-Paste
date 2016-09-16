1d69e0bb6684a101d62b2160421aee169049a0b9
Mjdgithuber <matthew@technecure.com>
2016-09-15 22:56:36 -0400

    Problem with the formating (no tabs) fixed.

--- src/FakeHuman.java
+++ src/FakeHuman.java
@@ -11,70 +11,68 @@ import java.util.Scanner;
  
 public class FakeHuman {
         	
-  	Random rd = new Random();
-  	Scanner scan = new Scanner(System.in);
-  	Robot r;
-  	int charsPM;//Chars per minute
-  	HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
-  	char currentChar;
-    char[] spChars = {'"' , ':', '?', '{', '}', '<', '>'};
-    char[] toChars = {'\'', ';', '/', '[', ']', ',', '.'};
-  	
-  	public FakeHuman(){
-        try {r = new Robot();}
-        catch (AWTException e) {e.printStackTrace();}
-  	}
+	Random rd = new Random();
+	Scanner scan = new Scanner(System.in);
+	Robot r;
+	int charsPM;//Chars per minute
+	HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
+	char currentChar;
+	char[] spChars = {'"' , ':', '?', '{', '}', '<', '>'};
+	char[] toChars = {'\'', ';', '/', '[', ']', ',', '.'};
+	
+	public FakeHuman(){
+		try {r = new Robot();}
+		catch (AWTException e) {e.printStackTrace();}
+	}
  
-  	public static void main(String[] args) throws InterruptedException{
-        FakeHuman f = new FakeHuman();
-        f.fillHashMap();
-        //f.input();
-        f.fileInput();
-  	}
+	public static void main(String[] args) throws InterruptedException{
+		FakeHuman f = new FakeHuman();
+		f.fillHashMap();
+		//f.input();
+		f.fileInput();
+	}
   	
-  	private void fillHashMap(){
-  		for(int i = 0; i<spChars.length; i++) hmap.put(spChars[i], KeyEvent.getExtendedKeyCodeForChar(toChars[i]));
-  	}
+	private void fillHashMap(){
+		for(int i = 0; i<spChars.length; i++) hmap.put(spChars[i], KeyEvent.getExtendedKeyCodeForChar(toChars[i]));
+	}
   	
-  	private boolean doesListContain(char[] array, char c){
+	private boolean doesListContain(char[] array, char c){
 		for(char n : array){
 			if(c == n) return true;
 		}
 		return false;
-  	}
+	}
   	
-  	private void fileInput() throws InterruptedException{
-        setCharsPerMinute(5000);
-        Thread.sleep(1000);
-        
-
-        
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Matthew Dennerlein\\Desktop\\Hello.txt"))){//BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Matthew Dennerlein\\Desktop\\Hello.txt"))
-          	String currentLine;
-           	while ((currentLine = br.readLine()) != null) {
-               	char[] sToChars = currentLine.toCharArray();
-               	
-                for(int i = 0; i<currentLine.length(); i++){
-                  	sleep();
-                  	
-                  	if(doesListContain(spChars, sToChars[i])) {
-                  		r.keyPress(KeyEvent.VK_SHIFT);
-                  		r.keyPress(hmap.get(sToChars[i]));
-                  		r.keyRelease(KeyEvent.VK_SHIFT);
-                  	}else{
-                      	if(Character.isUpperCase(sToChars[i])){
-                            	r.keyPress(KeyEvent.VK_SHIFT);
-                            	r.keyPress(KeyEvent.getExtendedKeyCodeForChar(sToChars[i]));
-                            	r.keyRelease(KeyEvent.VK_SHIFT);
-                      	}else {
-                      		r.keyPress(KeyEvent.getExtendedKeyCodeForChar(sToChars[i]));
-                      	}
-                	}
-            	}
-            	r.keyPress(KeyEvent.VK_ENTER);
-          	}
-    	} catch (IOException e) {e.printStackTrace();}
-  	}
+	private void fileInput() throws InterruptedException{
+		setCharsPerMinute(5000);
+		Thread.sleep(1000);
+		
+		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Matthew Dennerlein\\Desktop\\Hello.txt"))){//BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Matthew Dennerlein\\Desktop\\Hello.txt"))
+			String currentLine;
+			while ((currentLine = br.readLine()) != null) {
+				char[] sToChars = currentLine.toCharArray();
+				
+				for(int i = 0; i<currentLine.length(); i++){
+					sleep();
+					
+					if(doesListContain(spChars, sToChars[i])) {
+						r.keyPress(KeyEvent.VK_SHIFT);
+						r.keyPress(hmap.get(sToChars[i]));
+						r.keyRelease(KeyEvent.VK_SHIFT);
+					}else{
+						if(Character.isUpperCase(sToChars[i])){
+							r.keyPress(KeyEvent.VK_SHIFT);
+							r.keyPress(KeyEvent.getExtendedKeyCodeForChar(sToChars[i]));
+							r.keyRelease(KeyEvent.VK_SHIFT);
+						}else {
+							r.keyPress(KeyEvent.getExtendedKeyCodeForChar(sToChars[i]));
+						}
+					}
+				}
+				r.keyPress(KeyEvent.VK_ENTER);
+			}
+		} catch (IOException e) {e.printStackTrace();}
+	}
   	
 //  	private void input() throws InterruptedException{
 //        	setCharsPerMinute(120);
@@ -92,18 +90,18 @@ public class FakeHuman {
 //                    	r.keyRelease(KeyEvent.VK_SHIFT);
 //              	}else r.keyPress(KeyEvent.getExtendedKeyCodeForChar(sToChars[i]));
 //        	}
-//  	}
+//		}
   	
-  	private void setCharsPerMinute(int cps){ charsPM = cps; }
-  	
-  	private void sleep(){
-    	double milliStop = 1d/(charsPM*1d / 60d / 1000d); //60 for seconds per minute, and 1000 for milliseconds per second
-    	//long millis = (long) ((.75 + (rd.nextDouble()))*milliStop); //Test to generate human varibles
-    	long millis = (long) milliStop;
-    	try{
-          	Thread.sleep(millis);
-    	}catch(Exception e){ e.printStackTrace(); }
-  	}
+	private void setCharsPerMinute(int cps){ charsPM = cps; }
+	
+	private void sleep(){
+		double milliStop = 1d/(charsPM*1d / 60d / 1000d); //60 for seconds per minute, and 1000 for milliseconds per second
+		//long millis = (long) ((.75 + (rd.nextDouble()))*milliStop); //Test to generate human varibles
+		long millis = (long) milliStop;
+		try{
+			Thread.sleep(millis);
+		}catch(Exception e){ e.printStackTrace(); }
+	}
 }
 
 
