2411c412ec2bae9369ea319c269c103917a6e18e
Mjdgithuber <matthew@technecure.com>
2016-09-20 21:45:52 -0400

    Implemented backspacing to previous word to simulate more human errors
    More code organization and even more to come
    
    Many comments added to increase readability 
    
    Almost ready for release 1.0

--- src/KeySimulator.java
+++ src/KeySimulator.java
@@ -18,8 +18,12 @@ public class KeySimulator {
 	
 	int charsPM;//Chars per minute
 	
-	boolean backspace;
+	boolean wordNeedsBackspacing;
 	char actualKey;
+	
+	int spaceAt; //Used to determine were a word begins and ends
+	int currentCharNo;
+	char[] charsInLine;
 	
 	char[] spChars = {'"' , ':', '?', '{', '}', '<', '>', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'};
 	char[] toChars = {'\'', ';', '/', '[', ']', ',', '.', '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='};
@@ -50,7 +54,7 @@ public class KeySimulator {
 		return false;
 	}
 	
-	private void shiftChar(Robot robot, char c, boolean inHash){
+	private void shiftChar(char c, boolean inHash){
 		r.keyPress(KeyEvent.VK_SHIFT);
 		if(inHash) r.keyPress(hmap.get(c));
 		else r.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
@@ -58,41 +62,60 @@ public class KeySimulator {
 	}
   	
 	private void fileInput() throws InterruptedException{
-		setCharsPerMinute(800);//6000
-		Thread.sleep(2000);
-		
-		try (BufferedReader br = new BufferedReader(new FileReader("robotCopy.txt"))){//BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Matthew Dennerlein\\Desktop\\Hello.txt"))
-			String currentLine;
-			while ((currentLine = br.readLine()) != null) {
-				System.out.println(currentLine);
-				char[] sToChars = currentLine.toCharArray();
+		setCharsPerMinute(400);//This line of code sets the chars per minute (in the future this will be dictated by the user)
+		Thread.sleep(2000);//This will be removed later on (hard coded sleep)
+
+		try (BufferedReader br = new BufferedReader(new FileReader("robotCopy.txt"))){
+			String currentLine;//Makes a local string to store the chars
+			while ((currentLine = br.readLine()) != null) {//Will only go into loop if there is something on the line of the file
+				//System.out.println(currentLine);
+				char[] stringToChars = currentLine.toCharArray(); //Converts the current line into an array of chars
+				
+				charsInLine = stringToChars;//Records stringToChars in a global array so it can be used in other methods
 				
 				for(int i = 0; i<currentLine.length(); i++){
-					sleep();
-					
-					if(doesListContain(spChars, sToChars[i])) shiftChar(r, sToChars[i], true);
+					sleep();//Sleeps for a certain amount of time dictated by the setCharsPerMinute method
+
+					if(stringToChars[i] == ' ') spaceAt = i;//If there is a space than it records it (this is used to determine where the word begins)
+					currentCharNo = i;//This save the location in the array of the char we are currently dealing with
+
+					//If the char is one of the special chars (spChars at top of class) it will 'convert' it into it 'lowercase' and then shift it back
+					if(doesListContain(spChars, stringToChars[i])) shiftChar(stringToChars[i], true);
 					else{
-						if(Character.isUpperCase(sToChars[i])) shiftChar(r, sToChars[i], false);
+						//If the char is uppercase then we need to send it to be uppercased
+						if(Character.isUpperCase(stringToChars[i])) shiftChar(stringToChars[i], false);
 						else{
 							try{
-								char errorKey = typoError.testForError(sToChars[i]);
-								if(errorKey != sToChars[i]){
-									backspace = true;
-									actualKey = sToChars[i];
-								}
-								r.keyPress(KeyEvent.getExtendedKeyCodeForChar(errorKey));
-								//r.keyPress(KeyEvent.getExtendedKeyCodeForChar(sToChars[i]));
+								char possibleErrorKey = typoError.testForError(stringToChars[i]);//This generates a char that might be wrong (Human Error)
+								if(possibleErrorKey != stringToChars[i])//Checks to see if there was an error (this error is fake and on purpose!)
+									wordNeedsBackspacing = true;//Tells the program to backspace when it is time
+								r.keyPress(KeyEvent.getExtendedKeyCodeForChar(possibleErrorKey));//Types the key (error or not)
 							}
 							catch(Exception e){e.printStackTrace();}
 						}
 					}
 					
-					if(backspace){
-						backspace = false;
+					if(wordNeedsBackspacing){
+						wordNeedsBackspacing = false;
 						sleep();
-						r.keyPress(KeyEvent.VK_BACK_SPACE);
 						sleep();
-						try {r.keyPress(KeyEvent.getExtendedKeyCodeForChar(actualKey));}catch(Exception e){}
+						
+						int goBack = 0;
+						
+						for(int loops = 0; currentCharNo-loops > -1 && charsInLine[currentCharNo-loops] != ' '; loops++){
+							goBack++;
+							r.keyPress(KeyEvent.VK_BACK_SPACE);
+							sleep();
+						}
+						
+						for(int reWrite=0; reWrite<goBack; reWrite++){
+							if(doesListContain(spChars, charsInLine[currentCharNo-goBack+reWrite+1])) shiftChar(charsInLine[currentCharNo-goBack+reWrite+1], true);
+							else{
+								if(Character.isUpperCase(charsInLine[currentCharNo-goBack+reWrite+1])) shiftChar(charsInLine[currentCharNo-goBack+reWrite+1], false);
+								else r.keyPress(KeyEvent.getExtendedKeyCodeForChar(charsInLine[currentCharNo-goBack+reWrite+1]));
+							}
+							sleep();
+						}
 					}
 				}
 				r.keyPress(KeyEvent.VK_ENTER);
@@ -110,4 +133,12 @@ public class KeySimulator {
 			Thread.sleep(millis);
 		}catch(Exception e){ e.printStackTrace(); }
 	}
+	
+	private void sleep(int millis){
+		try{
+			Thread.sleep(millis);
+		}catch(Exception e){ e.printStackTrace(); }
+	}
 }
+
+
